d78c47fba45e74d095b5f6062029eeeed02352e7
Christian Dupuis <christian@dupuis.is>
2016-10-30 08:24:07 +0100

    Initial commit from the atomist bot

---
+++ src/com/atomist/compojure11/models/status.clj
@@ -0,0 +1,7 @@
+(ns com.atomist.compojure11.models.status
+  (:require [schema.core :as s]))
+
+(s/defschema Status
+  "The status of this service"
+  {:version s/Str
+   :status  (s/enum "OK" "WARN" "ERROR")})
